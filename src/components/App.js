921edcc1eafb5549d636ea5eafdad168bf87a746
Roberto Chupin <spartanZ11@SpartanZ11.local>
2019-06-13 17:10:41 -0500

    reduced divs

--- src/components/App.js
+++ src/components/App.js
@@ -2,43 +2,34 @@ import React, { useState } from "react";
 import SearchBar from "./SearchBar";
 import LocationList from "./LocationList";
 import LocationDetail from "./LocationDetail";
-
 import "./styles.css";
 
 const App = () => {
-  const [button, btnResource] = useState("list");
+  const [button, btnSet] = useState("list");
   return (
     <div className="ui-container">
       <div className="MoveRight">
         <div className="ui grid">
           <div className="ui row">
             <div className="column five wide">
-              <div>
-                <SearchBar />
-                <div className="purple ui buttons">
-                  <button
-                    className="ui button active"
-                    onClick={() => btnResource("list")}
-                  >
-                    List
-                  </button>
-                  <button
-                    className="ui button"
-                    onClick={() => btnResource("fav")}
-                  >
-                    Favorites
-                  </button>
-                </div>
+              <SearchBar />
+              <div className="purple ui buttons">
+                <button
+                  className="ui button active"
+                  onClick={() => btnSet("list")}
+                >
+                  List
+                </button>
+                <button className="ui button" onClick={() => btnSet("fav")}>
+                  Favorites
+                </button>
               </div>
               <br />
               <LocationList button={button} />
             </div>
             <div className="ten wide column">
-              <div>
-                <br />
-                <LocationDetail />
-              </div>
-              <div />
+              <br />
+              <LocationDetail />
             </div>
           </div>
         </div>
